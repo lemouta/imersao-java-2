fc1c68c0412d6d7a4c525f4cdddee84afcd241a8
Leandro Mouta <leandro@mouta.net>
2023-03-27 23:18:24 -0300

    Versão inicial

---
+++ src/App.java
@@ -0,0 +1,43 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        var client = HttpClient.newHttpClient();
+        
+        var req = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<String> res = client.send(req, BodyHandlers.ofString());
+
+        String body = res.body();
+
+        var parser = new JsonParser();
+        List<Map<String, String>> movieList = parser.parse(body);
+
+        for (Map<String, String> movie : movieList) {
+            printAttr("Título", movie.get("title"), false);
+            printAttr("Poster", movie.get("image"), false);
+            printAttr("Classificação", movie.get("imDbRating"), true);
+
+            var stars = (int) Math.floor(Double.valueOf(movie.get("imDbRating")));
+            for (int i = 0; i < stars; i++) {
+                System.out.print("\u2B50");
+            }
+
+            System.out.println();
+        }
+    }
+
+    private static void printAttr(String name, String val, Boolean colored) {
+        if (colored) {
+            System.out.print("\u001b[37m\u001b[44m");
+        }
+        
+        System.out.println(name + ": \u001b[1m" + val + "\u001b[m");
+    }
+}
